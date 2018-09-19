32dfb8ab82c4d4da7e2efd5552e6b1100f9f5a35
Maicon <msantana@assurehedge.com>
2018-09-19 17:52:14 +0100

    start to add oauth2

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,22 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+var OAuthServer = require('express-oauth-server');
+
 const danceClassController = require('./controllers/danceclass.controller');
 const userController = require('./controllers/user.controller');
 
 const app = express();
+app.oauth = new OAuthServer({
+    debug:true,
+    model: require('./models/oauth.model')
+});
 app.use(cors());
 app.use(bodyParser.json());
 let port = 80;
 function connect () {
-    return mongoose.connect('mongodb://kuppinguserusuario:123asd123z@mongo:27017/kupping',{
+    //return mongoose.connect('mongodb://kuppinguserusuario:123asd123z@mongo:27017/kupping',{
+    return mongoose.connect('mongodb://localhost:27017/kupping',{
         useNewUrlParser: true, 
         authSource:"admin" ,
         keepAlive: 1 
@@ -32,6 +39,8 @@ mongoose.connection.on('error', console.log)
     .once('open', listen);
 
 function listen(){
+
+    app.use(app.oauth.token());
     app.get('/public/danceclass', danceClassController.getDanceClasses);
     app.post('/public/user', userController.insertUser);
     
